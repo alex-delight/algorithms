373ad359cb972315efaccfb13acc5131ac9fe4a5
Терзи Александр <36917292+alex-delight@users.noreply.github.com>
2020-06-14 01:21:54 +0300

    Create binary_search.py
    Алгоритм бинарного поиска

---
+++ binary_search.py
@@ -0,0 +1,31 @@
+'''Алгоритм бинарного поиска'''
+
+'''Бинарный поиск = это поиск в массиве данных в середине списка с последующим делением на два.'''
+
+'''Например:'''
+'''128 = 64 = 32 = 16 = 8 = 4 = 2 = 1 # стандартное число шагов поиска - 7'''
+'''256 = 128 = 64 = 32 = 16 = 8 = 4 = 2 = 1 # если увеличить массив на два, шагов будет 8''''
+
+'''Код бинарного поиска в реализации Python:'''
+
+def binary_search(list, item):
+
+  low = 0
+  high = len(list)-1
+
+  while low <= high:
+    mid = (low + high)
+    guess = list[mid]
+    
+    if guess == item:
+      return mid
+    elif guess > item:
+      high = mid - 1
+    else:
+      low = mid + 1
+
+  return None
+
+my_list = [1, 3, 5, 7, 9]
+
+print(binary_search(my_list, 7))
